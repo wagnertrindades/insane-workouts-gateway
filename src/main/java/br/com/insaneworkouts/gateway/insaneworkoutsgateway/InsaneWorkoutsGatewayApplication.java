b0e5db9afedeb1d99fed8c7567c3a6ba33fc178e
wtrindades <wtrindades@gmail.com>
2022-02-14 09:53:27 -0300

    build: init Insane Workouts Gateway project

---
+++ src/main/java/br/com/insaneworkouts/gateway/insaneworkoutsgateway/InsaneWorkoutsGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.insaneworkouts.gateway.insaneworkoutsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsaneWorkoutsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsaneWorkoutsGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/insaneworkouts/gateway/insaneworkoutsgateway/InsaneWorkoutsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.insaneworkouts.gateway.insaneworkoutsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsaneWorkoutsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
